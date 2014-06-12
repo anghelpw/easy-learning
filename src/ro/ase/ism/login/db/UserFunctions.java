8b7ca90906bd5443d8a891181de0e014533bf93a
dementor53 <dan.anghel89@yahoo.com>
2014-06-12 08:17:11 +0300

    AESEncryption class
    Encryption method and cheange UserFunction class

--- src/ro/ase/ism/login/db/UserFunctions.java
+++ src/ro/ase/ism/login/db/UserFunctions.java
@@ -2,13 +2,17 @@ package ro.ase.ism.login.db;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONObject;
 
+//import android.util.Log;
+
 public class UserFunctions {
 
 	private JSONParser jsonParser;
+//	private AESEncryptionHelper encryptionHelper;
 
 	// URL of the PHP API
 	private static String loginURL = "http://practica.gdm.ro/danut/android_razvan/Functions.php";
@@ -21,9 +25,13 @@ public class UserFunctions {
 	private static String forpass_tag = "forpass";
 	private static String chgpass_tag = "chgpass";
 
+	// private static String encrypted = null;
+	// private static String decrypted = null;
+
 	// Constructor
 	public UserFunctions() {
 		jsonParser = new JSONParser();
+//		encryptionHelper = new AESEncryptionHelper();
 	}
 
 	/**
@@ -33,11 +41,40 @@ public class UserFunctions {
 	public JSONObject loginUser(String email, String password) {
 
 		// Building Parameters
+		
+//		try {
+//			email = AESEncryptionHelper.bytesToHex(encryptionHelper
+//					.encrypt(email));
+//			password = AESEncryptionHelper.bytesToHex(encryptionHelper
+//					.encrypt(password));
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		Log.v("CriptareEmail", email);
+//		Log.v("CriptareParola", password);
+		
 		List<NameValuePair> params = new ArrayList<NameValuePair>();
 		params.add(new BasicNameValuePair("tag", login_tag));
 		params.add(new BasicNameValuePair("email", email));
 		params.add(new BasicNameValuePair("password", password));
 		JSONObject json = jsonParser.getJSONFromUrl(loginURL, params);
+		return json;
+	}
+
+	/**
+	 * Function to Create Account
+	 **/
+
+	public JSONObject registerUser(String email, String username,
+			String password) {
+
+		// Building Parameters
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("tag", register_tag));
+		params.add(new BasicNameValuePair("email", email));
+		params.add(new BasicNameValuePair("username", username));
+		params.add(new BasicNameValuePair("password", password));
+		JSONObject json = jsonParser.getJSONFromUrl(registerURL, params);
 		return json;
 	}
 
@@ -64,23 +101,6 @@ public class UserFunctions {
 		params.add(new BasicNameValuePair("tag", forpass_tag));
 		params.add(new BasicNameValuePair("forgotpassword", forgotPassword));
 		JSONObject json = jsonParser.getJSONFromUrl(forpassURL, params);
-		return json;
-	}
-
-	/**
-	 * Function to Create Account
-	 **/
-
-	public JSONObject registerUser(String email, String username,
-			String password) {
-
-		// Building Parameters
-		List<NameValuePair> params = new ArrayList<NameValuePair>();
-		params.add(new BasicNameValuePair("tag", register_tag));
-		params.add(new BasicNameValuePair("email", email));
-		params.add(new BasicNameValuePair("username", username));
-		params.add(new BasicNameValuePair("password", password));
-		JSONObject json = jsonParser.getJSONFromUrl(registerURL, params);
 		return json;
 	}
 
